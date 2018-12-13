da2817198bd842ad363a0d6258120218afc7d1a1
Sibasish Tripathyy <sibasishtripathyy@gmail.com>
2018-12-13 23:39:55 +0530

    Added division

--- basic-calculator.py
+++ basic-calculator.py
@@ -14,6 +14,11 @@ def diff()
     b = int(input("Enter b: "))
     print("Sum: " + str(a * b))
 
+def diff()
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    print("Sum: " + str(a / b))
+
 print("Enter your choice")
 print("Press 1: Addition\n2: Subtraction\n3: Multiplication\n4: Division")
 choice = int(input())
