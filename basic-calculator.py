783c10a2e3cc13d176636828a9fd127eaab9abe4
Sibasish Tripathyy <sibasishtripathyy@gmail.com>
2018-12-13 22:27:01 +0530

    Added addition function

--- basic-calculator.py
+++ basic-calculator.py
@@ -1,3 +1,12 @@
+
+def sum():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    print("Sum: " + str(a + b))
+
 print("Enter your choice")
 print("Press 1: Addition\n2: Subtraction\n3: Multiplication\n4: Division")
-choice = input()
+choice = int(input())
+
+if choice == 1:
+    sum()
