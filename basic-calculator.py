0301c365e7fb031b7b0e9f67f69de861d8b2631c
Sibasish Tripathyy <sibasishtripathyy@gmail.com>
2019-06-07 21:31:24 +0530

    Fixed some basic errors

--- basic-calculator.py
+++ basic-calculator.py
@@ -7,21 +7,17 @@ def sum():
 def diff():
     a = int(input("Enter a: "))
     b = int(input("Enter b: "))
-    print("Sum: " + str(a - b))
+    print("Difference: " + str(a - b))
 
 def mult():
     a = int(input("Enter a: "))
     b = int(input("Enter b: "))
-    print("Sum: " + str(a * b))
+    print("Product: " + str(a * b))
 
 def div():
     a = int(input("Enter a: "))
     b = int(input("Enter b: "))
-    print("Sum: " + str(a * b))
-
-a = int(input("Enter a: "))
-b = int(input("Enter b: "))
-print("Sum: " + str(a / b))
+    print("Division: " + str(a / b))
 
 print("Enter your choice")
 print("Press 1: Addition\n2: Subtraction\n3: Multiplication\n4: Division")
@@ -31,3 +27,7 @@ if choice == 1:
     sum()
 elif choice == 2:
     diff()
+elif choice == 3:
+    mult()
+elif choice == 4:
+    div()
